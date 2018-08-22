5d72dd9fdc63bfb9a72f1002a308adb1bd87caf7
Florian Wessner <florian.wessner@surfmedia.de>
2018-08-22 13:54:22 +0200

    handle scrolling in the modern way

--- resources/js/containers/MainContainer.jsx
+++ resources/js/containers/MainContainer.jsx
@@ -6,46 +6,92 @@ import MainNav from "./MainNav";
 class Component extends React.Component {
     constructor(props) {
         super(props);
-        let {home} = navigation;
         this.state = {
-            containerMarginTop: 0,
-            triangleClassName:'home',
-            triangleTxt: home.label,
-        }
+            activePanelIndex: 0,
+            triangleClassName: 'hidden',
+            triangleTxt: null,
+        };
+        this.panelCount = Object.keys(navigation).length;
+        this.scrollHandler = 'scrollBehavior' in document.documentElement.style ? this.scrollIntoView : this.animateScrolling;
+        this.scrollTimeout = null;
+
+        this.addEvents();
+        this.checkLocation(this.props.location.pathname);
     }
 
-    componentDidMount(){
-        this.appContainer = document.getElementById('app');
+    addEvents() {
+        let prefix = !('onwheel' in document.createElement('div')) ? 'mouse' : '';
+
+        window.addEventListener('scroll', this.onScroll);
+        document.addEventListener(`${prefix}wheel`, this.onMouseWheel); //IE9, Chrome, Safari, Opera
     }
 
     componentWillUpdate(nextProps) {
         if (this.props.location.pathname !== nextProps.location.pathname) {
-            let target = nextProps.location.pathname.split('/')[1];
-            let keyPos = Object.keys(navigation).indexOf(target);
-
-            if (target && keyPos !== -1) {
-                let navigationObj = navigation[target];
-                let panel = document.getElementById(`${target}Panel`);
-                window.scrollTo(0, window.innerHeight * keyPos);
-
-                setTimeout(()=>{
-                    this.setState({
-                        triangleClassName: target,
-                        triangleTxt: navigationObj.label
-                    });
-                }, 200)
-            }
+            this.checkLocation(nextProps.location.pathname);
         }
     }
+
+    checkLocation(pathname) {
+        let key = pathname.split('/')[1];
+        if(key && navigation[key]){
+            this.setActivePanel(key);
+        }
+    }
+
+    setActivePanel(key) {
+        let keyPos = Object.keys(navigation).indexOf(key);
+        this.setState({activePanelIndex: keyPos});
+
+        let navigationObj = navigation[key];
+        this.scrollHandler(key);
+
+        setTimeout(() => {
+            this.setState({
+                triangleClassName: key,
+                triangleTxt: navigationObj.label
+            });
+        }, 200);
+    }
+
+    animateScrolling(key) {
+        console.log('animateScrolling', key);
+    }
+
+    scrollIntoView(key) {
+        let panel = document.getElementById(`${key}Panel`);
+        if(panel){
+            panel.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+        }
+    }
+
+    onScroll = (e) => {
+//        console.log(e);
+//        e.preventDefault();
+    };
+
+    onMouseWheel = (e) => {
+        let value = e.wheelDelta || -e.deltaY || -e.detail;
+        let direction = Math.max(-1, Math.min(1, value)) * -1;
+
+        window.clearTimeout(this.scrollTimeout);
+        this.scrollTimeout = window.setTimeout(() => {
+            let index = this.state.activePanelIndex + direction;
+            if(index >= 0 && index < this.panelCount ) {
+                this.setActivePanel(Object.keys(navigation)[index]);
+            }
+        }, 300);
+        e.preventDefault();
+    };
 
     render() {
         let {triangleClassName, triangleTxt} = this.state;
         return ([
-            <MainNav key={'mainNav'} />,
+            <MainNav key={'mainNav'}/>,
             <div className={`mainTriangle ${triangleClassName}`} key={'centerTriangle'}>
                 <h1>{triangleTxt}</h1>
-                <div className="triangle outline"> </div>
-                <div className="triangle area"> </div>
+                <div className="triangle outline"></div>
+                <div className="triangle area"></div>
             </div>,
             <main key={'main'}>
                 {this.props.children}
